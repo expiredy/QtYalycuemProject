42eb045aba77e8695e68305fc31475379f53f923
Ярослав <62770020+expiredy101@users.noreply.github.com>
2020-10-24 12:51:35 +0300

    Delete activation.py

--- activation.py
+++
@@ -1,6 +0,0 @@
-import interface
-import bot_code
-#token: NzY0ODUwMTg0MDE4MzI5NjAw.X4MQPg.FbDc41UWAsYb8WyjNCaQ1FowY_o
-# ______________________Design's strat___________________
-interface.activate()
-# bot_code.activate("NzY0ODUwMTg0MDE4MzI5NjAw.X4MQPg.FbDc41UWAsYb8WyjNCaQ1FowY_o")
